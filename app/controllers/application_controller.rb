f56fd56c2868603ecc7bfe7b13c889343004fb8f
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-10-21 10:18:51 -0400

    Getting devise working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def current_coach
+    if current_user.present?
+      current_user
+    end
+  end
+  
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,
+        keys: [:username, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:sign_in,
+        keys: [:login, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:account_update,
+        keys: [:username, :email, :password_confirmation, :current_password])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :team
+  has_many :players, through: :team
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
